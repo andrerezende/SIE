908653d32bacd16d997780d05a6b7e7fb0d0f621
saulo.leal <saulo.leal@GTI0000005.reitoria.local>
2011-09-27 15:23:12 -0300

    Criação da classe UnidadeFederativa

---
+++ administracao/classes/UnidadeFederativa.php
@@ -0,0 +1,50 @@
+<?php
+class UnidadeFederativa {
+	protected $id;
+	protected $nome;
+        protected $pais_id;
+
+	public function UnidadeFederativa($pid = null, $pnome = null, $ppaisid = null) {
+		$this->id = $pid;
+		$this->nome = $pnome;
+	}
+
+	public function getIdUnidadeFederativa() {
+		return $this->id;
+	}
+
+	public function setIdUnidadeFederativa($pid) {
+		$this->id = $pid;
+	}
+
+	public function getNome() {
+		return $this->nome;
+	}
+
+	public function setNome($pnome) {
+		$this->nome = $pnome;
+	}
+        
+        public function getIdPais() {
+		return $this->pais_id;
+	}
+
+	public function setIdPais($ppaisid) {
+		$this->pais_id = $ppaisid;
+	}
+
+	public function SelectByAll($sock) {
+		$ssql = "SELECT id, descricao FROM unidade_federativa A " ;
+		$ssql .= " ORDER BY descricao ASC";
+		$rs = mysql_query($ssql, $sock);
+
+		$ar = array();
+
+		while ($linha = mysql_fetch_row($rs)){
+			$obj = new UnidadeFederativa($linha[0], $linha[1]);
+			$ar[] = $obj;
+		}
+		return ($ar);
+	}
+
+}
