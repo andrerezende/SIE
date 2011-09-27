1c986409f6bdd3c63dc24d7559a9d3cd436fca2a
saulo.leal <saulo.leal@GTI0000005.reitoria.local>
2011-09-27 15:24:22 -0300

    Consulta municipios para jquery

---
+++ administracao/inscrito/municipios.php
@@ -0,0 +1,24 @@
+<?php session_start("SELECAO"); ?>
+<?php
+header("Content-Type: text/html; charset=ISO-8859-1", true);
+/* Acesso ao banco de dados */
+
+include ("../classes/DB.php");
+include ("../classes/UnidadeFederativa.php");
+
+$banco   = DB::getInstance();
+$conexao = $banco->ConectarDB();
+
+$uf = addslashes($_POST['uf']);
+
+$sql = "SELECT * FROM municipio WHERE unidade_federativa_id = '$uf' ORDER BY nome ASC";
+$qr = $banco->ExecutaQueryGenerica($sql);
+
+if (mysql_num_rows($qr) == 0) {
+	echo '<option value="0">'.htmlentities('Nao existem cidades nesse Estado').'</option>';
+} else {
+	while($ln = mysql_fetch_assoc($qr)){
+		echo '<option value="'.$ln['id'].'">'.$ln['nome'].'</option>';
+	}
+}
+?>
