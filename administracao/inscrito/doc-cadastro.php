af30a1ff9a0ffcc35063adf76d7099004c7ed087
saulo.leal <saulo.leal@GTI0000005.reitoria.local>
2011-10-11 11:59:54 -0300

    Em manutençao...

--- administracao/inscrito/doc-cadastro.php
+++ administracao/inscrito/doc-cadastro.php
@@ -11,9 +11,7 @@
 	if ($data_fim < $data_atual){
 		header("Location: ../../index.php?sc=Inscricao");
 	}
-
 ?>
-
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
