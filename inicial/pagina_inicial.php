2121b32d2a5c0ba231690f43f094e82534349a0e
Saulo Leal <Saulo Leal@PCSAULO>
2011-10-16 09:21:33 -0300

    Ok!

--- inicial/pagina_inicial.php
+++ inicial/pagina_inicial.php
@@ -1,5 +1,5 @@
 <?php
-session_start("SELECAO"); //sempre session_start antes de usar sessions
+// session_start("SELECAO"); //sempre session_start antes de usar sessions
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
