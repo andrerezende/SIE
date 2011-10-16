6244767920221c67cc297058532032c591c62971
Saulo Leal <Saulo Leal@PCSAULO>
2011-10-16 09:20:10 -0300

    Ok!

--- administracao/inscrito/cursos.php
+++ administracao/inscrito/cursos.php
@@ -18,7 +18,7 @@ if (mysql_num_rows($qr) == 0) {
 	echo '<option value="0">'.htmlentities('Nao existem cursos nesse Campus').'</option>';
 } else {
 	while($ln = mysql_fetch_assoc($qr)){
-		echo '<option value="'.$ln['cod_curso'].'">'.$ln['nome'].'</option>';
+		echo '<option value="'.$ln['cod_curso'].'">'.strtoupper($ln['nome']).'</option>';
 	}
 }
 ?>
