b11e16b7bf0e4f4a433d9adb8b0397013f5beb29
saulo.leal <saulo.leal@GTI0000005.reitoria.local>
2011-10-19 16:32:56 -0200

    Alterados parâmetros das sessões.

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 //Variáveis de sessão para parametrização - Serão extraídas do Banco de Dados
 
 session_start("SELECAO");
-$_SESSION["Gnomeprocessoseletivo"]    = "Processo Seletivo Unificado para Alunos - 2012";
+$_SESSION["Gnomeprocessoseletivo"]    = "Processo Seletivo Unificado para Estudantes - 2012";
 $_SESSION["Gdatainicio"]            = mktime(0, 0, 0, 8, 23, 2011); // 0,0,0, M/D/Y
 $_SESSION["Gdatatermino"]           = mktime(0, 0, 0, 11, 05, 2011);
-$_SESSION["Gdataterminoisencao"]    = mktime(0, 0, 0, 10, 01, 2011);
+$_SESSION["Gdataterminoisencao"]    = mktime(0, 0, 0, 10, 10, 2011);
 
-$_SESSION["Gvalorboleto"] = "50,00";
-$_SESSION["Gconvenio"] = "1234567";
+$_SESSION["Gvalorboleto"] = "10,00";
+$_SESSION["Gconvenio"] = "2203078";
 $_SESSION["Gedital"] = "130";
 $_SESSION["Gano"] = "2011";
-$_SESSION["Gpaginaconcurso"] = "http://www.ifbaiano.edu.br/concursos/portal/temporario20112/";
+$_SESSION["Gpaginaconcurso"] = "http://www.ifbaiano.edu.br/concursos/portal/discente2012";
 $_SESSION["Gusrmail"] = "temporario@ifbaiano.edu.br";
 $_SESSION["Gpwdmail"] = "temp";
 
