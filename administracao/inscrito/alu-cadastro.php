b9e3813b1f7f3b9837c5666b20e2ca90211559d4
Saulo Leal <saulo.leal@ifbaiano.edu.br>
2012-10-08 15:02:30 -0300

    Retirada obrigatoriedade da data de expedição.

--- administracao/inscrito/alu-cadastro.php
+++ administracao/inscrito/alu-cadastro.php
@@ -159,15 +159,18 @@ if ($data_fim < $data_atual) {
                     alert('Informe corretamente a data de nascimento!');
                     dataNascimento.focus();
                     resultado = false;
-                } else if (dataExpedicao.value == "") {
-                    alert('Informe a data de expedicao!');
-                    dataExpedicao.focus();
-                    resultado = false;
-                } else if (!CheckDate(dataExpedicao)) {
-                    alert('Informe corretamente a data de expedicao!');
-                    dataExpedicao.focus();
-                    resultado = false;
-                } else if(sexo.value == "") {
+                } 
+//                else if (dataExpedicao.value == "") {
+//                    alert('Informe a data de expedicao!');
+//                    dataExpedicao.focus();
+//                    resultado = false;
+//                } else if (!CheckDate(dataExpedicao)) {
+//                    alert('Informe corretamente a data de expedicao!');
+//                    dataExpedicao.focus();
+//                    resultado = false;
+//                }
+
+                else if(sexo.value == "") {
                     alert('Informe sexo!');
                     sexo.focus();
                     resultado = false;
